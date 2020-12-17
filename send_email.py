85f572e683faf66707515407f1d2500a198e4f76
ElPapi42 <whitman-2@hotmail.com>
2020-12-17 18:12:53 -0400

    Add requirements.txt and better organize the script code

--- send_email.py
+++ send_email.py
@@ -1,5 +1,9 @@
 import sys
 import csv
+import os
+
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Email, To, Content, Mail
 
 emails = []
 
@@ -8,21 +12,17 @@ with open(sys.argv[1], newline='') as csvfile:
     for row in reader:
         emails.append(row['email'])
 
-print(emails)
+sg = SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
 
-"""
-import sendgrid
-import os
-from sendgrid.helpers.mail import *
-
-sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
 from_email = Email("test@example.com")
 to_email = To("test@example.com")
 subject = "Sending with SendGrid is Fun"
 content = Content("text/plain", "and easy to do anywhere, even with Python")
+
 mail = Mail(from_email, to_email, subject, content)
+
 response = sg.client.mail.send.post(request_body=mail.get())
+
 print(response.status_code)
 print(response.body)
 print(response.headers)
-"""
