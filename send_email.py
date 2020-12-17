5e984205dccca209e9fed4b5b464240bad046da8
ElPapi42 <whitman-2@hotmail.com>
2020-12-17 18:02:43 -0400

    Basic email read script from cv file

---
+++ send_email.py
@@ -0,0 +1,28 @@
+import sys
+import csv
+
+emails = []
+
+with open(sys.argv[1], newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        emails.append(row['email'])
+
+print(emails)
+
+"""
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+
+sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+from_email = Email("test@example.com")
+to_email = To("test@example.com")
+subject = "Sending with SendGrid is Fun"
+content = Content("text/plain", "and easy to do anywhere, even with Python")
+mail = Mail(from_email, to_email, subject, content)
+response = sg.client.mail.send.post(request_body=mail.get())
+print(response.status_code)
+print(response.body)
+print(response.headers)
+"""
