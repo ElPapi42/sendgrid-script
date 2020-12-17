34fe36168c038a4bf3436f5a688f73bc60c0c758
ElPapi42 <whitman-2@hotmail.com>
2020-12-17 19:02:32 -0400

    Parametrize subject and sender

--- send_email.py
+++ send_email.py
@@ -7,20 +7,20 @@ from sendgrid.helpers.mail import Email, To, Content, Mail
 
 emails = []
 
-with open(sys.argv[1], newline='') as csvfile:
+with open(sys.argv[2], newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         emails.append(row['email'])
 
 template = '<h1>You dont supply an html template to the script</h1>'
 
-with open(sys.argv[2], 'r') as htmlfile:
+with open(sys.argv[4], 'r') as htmlfile:
     template = htmlfile.read()
 
 sg = SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
 
-from_email = Email('whitman-2@hotmail.com')
-subject = 'Sending with SendGrid is Fun'
+from_email = Email(sys.argv[1])
+subject = sys.argv[3]
 content = Content('text/html', template)
 
 mail = Mail(from_email, emails, subject, content)
